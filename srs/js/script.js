35005f456ec335acc8a7391ebed5dfe152a13d14
Valentina <111182566+ValentinaMol@users.noreply.github.com>
2023-10-15 00:36:55 +0300

    first commit

---
+++ srs/js/script.js
@@ -0,0 +1,7 @@
+$(".slider").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: '<button type="button" class="btn_arrow_right"></button>',
+  prevArrow: '<button type="button" class="btn_arrow_left"></button>',
+});
