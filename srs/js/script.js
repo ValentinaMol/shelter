65e10d6196efdaf432dcb80f53c9c4aa778e92d9
unknown <vinchevskaja15@gmail.com>
2023-10-16 00:08:10 +0300

    slider

--- srs/js/script.js
+++ srs/js/script.js
@@ -2,6 +2,4 @@ $(".slider").slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 3,
-  prevArrow: '<button type="button" class="btn_arrow_right"></button>',
-  prevArrow: '<button type="button" class="btn_arrow_left"></button>',
 });
